f6cec9552bbbf98a6bd3afca1927859e867ad5ce
SmithBWare <Smithwrestling89@gmail.com>
2020-07-28 13:07:18 -0400

    create webpage using file system and import function with module

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
 
 // const printProfileData = (profileDataArr) => {
 //     console.log(profileDataArr);
 // };
 
 // Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    };
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     };
 
-    console.log('================');
+//     console.log('================');
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+
+const fs = require('fs');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+const generatePage = require('./src/page-template');
+
+fs.writeFile('./index.html', generatePage, err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
